62ddb9817d5418c7f7d02dbca6674a8b51d39e9b
Li penghui <lipenghui@caxins.com>
2019-07-09 11:13:48 +0800

    自定义样式

--- config-overrides.js
+++ config-overrides.js
@@ -1,10 +1,17 @@
-const { override, fixBabelImports } = require('customize-cra')
+const { override, fixBabelImports, addLessLoader } = require('customize-cra')
     
 module.exports = override(
     // 针对antd实现按需打包,根据import来打包(使用babel-plugin-import)
     fixBabelImports('import', {
         libraryName: 'antd',
         libraryDirectory: 'es',
-        style:'css'
+        style: true, //自动打包相关文件
+    }),
+    //使用less loader对源码的覆盖，重新指定，自定义样式
+    addLessLoader({
+        javascriptEnabled: true,
+        modifyVars: {
+            '@primary-color':'#1da57a'
+        }
     })
 )
