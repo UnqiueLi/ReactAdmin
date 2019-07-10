efbddff3d51afc782200cc6dfc3b18275c619664
Li penghui <lipenghui@caxins.com>
2019-07-10 15:35:01 +0800

    高阶函数、高阶组件

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -64,7 +64,20 @@ class Login extends Component {
 }
 /* 
 1、高阶函数
+    一类特别的函数
+    a 接受函数类型的参数
+    b 返回值是函数
+    常见
+    a 定时器 setTimeout() / setInterval()
+    b Promise:Promise(()=>{} then(value=>{},reason=>{}))
+    c 数据遍历的相关方法: forEach()/filter()/map()/reduce()/find()/findIndex()
+    d 函数对象的bind()
+    e Form.create()() / getFieldDecorator()()
 2、高阶组件
+    a 本质就是一个函数
+    b 接收一个组件（被包装组件），返回一个新的组件（包装组件），新组件内部渲染被包装，包装组件会向被包装组件传入特定属性
+    c 作用：扩展组件的功能
+    d 高阶组件也是高阶函数:接收一个组件函数，返回一个新的组件函数
 */
 
 export default Form.create()(Login)
